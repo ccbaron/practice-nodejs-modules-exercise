d726f9471a8b0bf2bf30b7243fe4cedbb0d52378
ccbaron <christian7baron@gmail.com>
2025-05-24 16:04:59 +0200

    OK

--- 1-os-module.js
+++ 1-os-module.js
@@ -1,2 +1,17 @@
 // Leer README
 
+// Import the 'fs' module
+const fs = require('fs');
+// Import the 'path' module
+const path = require('path');
+// Import the 'os' module
+const os = require('os');
+
+//Nombre del equipo
+//Edición/Versión del sistema operativo
+//Memoria ram total en GB
+
+console.log('Nombre del equipo: ', os.hostname());
+console.log('Edición/Versión del sistema operativo: ', os.release());
+console.log('Memoria ram total en GB: ', os.totalmem() / 1024 / 1024 / 1024);
+
--- 2-get-random-fruit.js
+++ 2-get-random-fruit.js
@@ -2,3 +2,8 @@ const frutas = ["Manzana", "Banana", "Pera", "Naranja", "Uva", "Melón", "Sandí
     "Cereza", "Ciruela", "Mango", "Limón", "Mandarina", "Granada", "Higo", "Dátil", "Albaricoque", "Melocotón"];
 
 // importa el módulo "utils.js" y usa el método getRandomElementFromArray adecuadamente
+
+const { getRandomElementFromArray } = require('./mis-utilidades/utils.js');
+const randomFruit = getRandomElementFromArray(frutas, 3);
+console.log(`Fruta aleatoria: ${randomFruit}`);
+
